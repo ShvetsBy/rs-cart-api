c576522770937e4c04d3a624462ec702e1d700c6
Ivan Shvets <shvets.ivan@gmail.com>
2022-11-03 14:44:44 +0300

    update relations

--- src/database/entities/cart.entity.ts
+++ src/database/entities/cart.entity.ts
@@ -1,8 +1,9 @@
-import {Column, Entity, PrimaryColumn } from 'typeorm'
+import {Column, Entity, JoinColumn, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm'
+import { CartItem } from './cartItem.entity';
 
 @Entity()
 export class Cart {
-    @PrimaryColumn('uuid')
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column({type: 'date', nullable: false})
@@ -10,6 +11,10 @@ export class Cart {
 
     @Column({type: 'date', nullable: false})
     updated_at: Date;
+
+    @OneToMany(() => CartItem, items => items.cart)
+    @JoinColumn()
+    cart_item: CartItem[];
 }
 
 
